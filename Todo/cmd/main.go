9bd08d58f199f54c68a530642b2d756054209678
b1adeDEV <denistv2030@gmail.com>
2025-02-01 21:46:57 +0500

    create handlers for endpoints

--- Todo/cmd/main.go
+++ Todo/cmd/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"todo"
+	"todo/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler )
 	server := new(todo.Server)
-	if err := server.Run("8000"); err != nil {
+	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 }

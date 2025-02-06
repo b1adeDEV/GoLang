9b2d3cf3efff9087bbf672097c7a86eb7403af6f
b1adeDEV <denistv2030@gmail.com>
2025-02-06 21:41:39 +0500

    upgrade main.go

--- Todo/cmd/main.go
+++ Todo/cmd/main.go
@@ -4,12 +4,30 @@ import (
 	"log"
 	"todo"
 	"todo/pkg/handler"
+	"todo/pkg/repository"
+	"todo/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
-	handlers := new(handler.Handler )
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error initializing configs: %s", err.Error())
+	}
+
+	repo := repository.NewRepository()
+	services := service.NewService(repo)
+	handlers := handler.NewHandler(services)
+
 	server := new(todo.Server)
-	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigFile("config")
+	return viper.ReadInConfig()
 }
